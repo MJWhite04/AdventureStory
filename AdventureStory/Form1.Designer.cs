92524f45b5c9e91eccf95e06f1d4c40e3f2dd316
michwhit303 <michwhit303@SDS-L-F93HC63>
2024-10-23 11:12:47 -0400

    Add project files.

---
+++ AdventureStory/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace AdventureStory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.option1Label = new System.Windows.Forms.Label();
+            this.option2Label = new System.Windows.Forms.Label();
+            this.option3Label = new System.Windows.Forms.Label();
+            this.option1Button = new System.Windows.Forms.Button();
+            this.option2Button = new System.Windows.Forms.Button();
+            this.option3Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox
+            // 
+            this.imageBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox.Location = new System.Drawing.Point(87, 53);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(212, 178);
+            this.imageBox.TabIndex = 0;
+            this.imageBox.TabStop = false;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(12, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(81, 18);
+            this.outputLabel.TabIndex = 1;
+            this.outputLabel.Text = "Welcome";
+            // 
+            // option1Label
+            // 
+            this.option1Label.AutoSize = true;
+            this.option1Label.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option1Label.ForeColor = System.Drawing.Color.White;
+            this.option1Label.Location = new System.Drawing.Point(70, 253);
+            this.option1Label.Name = "option1Label";
+            this.option1Label.Size = new System.Drawing.Size(52, 18);
+            this.option1Label.TabIndex = 2;
+            this.option1Label.Text = "Label";
+            // 
+            // option2Label
+            // 
+            this.option2Label.AutoSize = true;
+            this.option2Label.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option2Label.ForeColor = System.Drawing.Color.White;
+            this.option2Label.Location = new System.Drawing.Point(70, 292);
+            this.option2Label.Name = "option2Label";
+            this.option2Label.Size = new System.Drawing.Size(52, 18);
+            this.option2Label.TabIndex = 3;
+            this.option2Label.Text = "Label";
+            // 
+            // option3Label
+            // 
+            this.option3Label.AutoSize = true;
+            this.option3Label.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.option3Label.ForeColor = System.Drawing.Color.White;
+            this.option3Label.Location = new System.Drawing.Point(70, 333);
+            this.option3Label.Name = "option3Label";
+            this.option3Label.Size = new System.Drawing.Size(52, 18);
+            this.option3Label.TabIndex = 4;
+            this.option3Label.Text = "Label";
+            // 
+            // option1Button
+            // 
+            this.option1Button.Location = new System.Drawing.Point(30, 253);
+            this.option1Button.Name = "option1Button";
+            this.option1Button.Size = new System.Drawing.Size(38, 23);
+            this.option1Button.TabIndex = 5;
+            this.option1Button.UseVisualStyleBackColor = true;
+            this.option1Button.Click += new System.EventHandler(this.option1Button_Click);
+            // 
+            // option2Button
+            // 
+            this.option2Button.Location = new System.Drawing.Point(30, 292);
+            this.option2Button.Name = "option2Button";
+            this.option2Button.Size = new System.Drawing.Size(37, 23);
+            this.option2Button.TabIndex = 6;
+            this.option2Button.UseVisualStyleBackColor = true;
+            this.option2Button.Click += new System.EventHandler(this.option2Button_Click);
+            // 
+            // option3Button
+            // 
+            this.option3Button.Location = new System.Drawing.Point(30, 332);
+            this.option3Button.Name = "option3Button";
+            this.option3Button.Size = new System.Drawing.Size(38, 23);
+            this.option3Button.TabIndex = 7;
+            this.option3Button.UseVisualStyleBackColor = true;
+            this.option3Button.Click += new System.EventHandler(this.option3Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(406, 374);
+            this.Controls.Add(this.option3Button);
+            this.Controls.Add(this.option2Button);
+            this.Controls.Add(this.option1Button);
+            this.Controls.Add(this.option3Label);
+            this.Controls.Add(this.option2Label);
+            this.Controls.Add(this.option1Label);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.imageBox);
+            this.Name = "Form1";
+            this.Text = "Adventure Story";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label option1Label;
+        private System.Windows.Forms.Label option2Label;
+        private System.Windows.Forms.Label option3Label;
+        private System.Windows.Forms.Button option1Button;
+        private System.Windows.Forms.Button option2Button;
+        private System.Windows.Forms.Button option3Button;
+    }
+}
+
---
+++ AdventureStory/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace AdventureStory
+{
+    
+    public partial class Form1 : Form
+    {
+        //Starting Page
+        int page = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            outputLabel.Text = "Welcome";
+            option1Label.Text = "...";
+            option2Label.Text = "...";
+            option3Label.Text = "...";
+        }
+
+        private void option1Button_Click(object sender, EventArgs e)
+        {
+            //Check what page we are on, then flip to the next page in the squence
+
+            if (page == 1)
+            {
+                page = 2;
+            }
+            else if (page == 2)
+            {
+                page = 99;
+            }
+            else if (page == 3)
+            {
+                page = 5;
+            }
+            else if (page == 5)
+            {
+                page = 6;
+            }
+            else if (page == 6)
+            {
+                page = 99;
+            }
+            else if (page == 4)
+            {
+                page = 99;
+            }
+            else if (page == 8)
+            {
+                page = 9;
+            }
+            else if (page == 9)
+            {
+                page = 11;
+            }
+            else if (page == 11)
+            {
+                page = 13;
+            }
+            else if (page == 13)
+            {
+                page = 100;
+            }
+            DisplayPage();
+        }
+
+        private void option2Button_Click(object sender, EventArgs e)
+        {
+            //Checks to see what page it is on then flips to the next page
+            if (page == 1)
+            {
+                page = 99;
+            }
+            else if (page == 2)
+            {
+                page = 3;
+            }
+            else if (page == 3)
+            {
+                page = 4;
+            }
+            else if (page == 4)
+            {
+                page = 6;
+            }
+            else if (page == 8)
+            {
+                page = 99;
+            }
+            else if (page == 9)
+            {
+                page = 10;
+            }
+            else if (page == 11)
+            {
+                page = 6;
+            }
+            DisplayPage();
+        }
+
+        private void option3Button_Click(object sender, EventArgs e)
+        {
+            if (page == 2)
+            {
+                page = 99;
+            }
+            else if (page == 5)
+            {
+                page = 4;
+            }
+            else if (page == 4)
+            {
+                page = 8;
+            }
+            else if (page == 8)
+            {
+                page = 99;
+            }
+            else if (page == 9)
+            {
+                page = 10;
+            }
+            else if (page == 11)
+            {
+                page = 14;
+            }
+            else if (page == 14)
+            {
+                page = 99;
+            }
+            DisplayPage();
+        }
+        private void DisplayPage()
+        {
+            //All the case statements(shows whats on the screen)
+            switch (page)
+            {
+                case 1:
+                    outputLabel.Text = "You are Trapped, Play Game?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    option3Label.Text = "...";
+                    break;
+                case 2:
+                    outputLabel.Text = "Level 1: What country has the largest land mass";
+                    option1Label.Text = "Canada";
+                    option2Label.Text = "Russia";
+                    option3Label.Text = "USA";
+                    break;
+                case 3:
+                    outputLabel.Text = "Beat level 1/3";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    option3Label.Text = "...";
+                    break;
+                case 4:
+                    outputLabel.Text = "You hear something in the woods what do you do?";
+                    option1Label.Text = "Investigate";
+                    option2Label.Text = "Run";
+                    option3Label.Text = "Go in castle";
+                    break;
+                case 5:
+                    outputLabel.Text = "You are thirsty, Take a Drink?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    option3Label.Text = "...";
+                    break;
+                case 6:
+                    outputLabel.Text = "Water was infected and you died.";
+                    option1Label.Text = "...";
+                    option2Label.Text = "...";
+                    option3Label.Text = "...";
+                    break;
+                case 7:
+                    outputLabel.Text = "It was a bear and you died.";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    option3Label.Text = "...";
+                    break;
+                case 8:
+                    outputLabel.Text = "You are faced with level 2. ?";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    option3Label.Text = "...";
+                    break;
+                case 9:
+                    outputLabel.Text = "Level 2 complete";
+                    option1Label.Text = "...";
+                    option2Label.Text = "...";
+                    option3Label.Text = "...";
+                    break;
+                case 10:
+                    outputLabel.Text = "Level 2 complete";
+                    option1Label.Text = "...";
+                    option2Label.Text = "...";
+                    option3Label.Text = "...";
+                    break;
+                case 11:
+                    outputLabel.Text = "Level 2 complete";
+                    option1Label.Text = "...";
+                    option2Label.Text = "...";
+                    option3Label.Text = "...";
+                    break;
+                case 13:
+                    outputLabel.Text = "Level 2 complete";
+                    option1Label.Text = "...";
+                    option2Label.Text = "...";
+                    option3Label.Text = "...";
+                    break;
+                case 14:
+                    outputLabel.Text = "Level 2 complete";
+                    option1Label.Text = "...";
+                    option2Label.Text = "...";
+                    option3Label.Text = "...";
+                    break;
+                case 99:
+                    outputLabel.Text = "Wrong Choice. Play Again";
+                    option1Label.Text = "Yes";
+                    option2Label.Text = "No";
+                    option3Label.Text = "...";
+                    break;
+                case 100:
+                    outputLabel.Text = "Thanks for Playing";
+                    option1Label.Text = "...";
+                    option2Label.Text = "...";
+                    option3Label.Text = "...";
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
